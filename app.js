55db9f49325671ad44eb95ad95ecddc86e0d7a76
Kubi Kubisiak <mckubisiak@gmail.com>
2021-04-28 17:04:45 -0700

    created basic calculator

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-// import functions and grab DOM elements
 
-// initialize state
+const firstInput = document.getElementById('input1');
+const secondInput = document.getElementById('input2');
+const addPress = document.getElementById('add');
+const subtractPress = document.getElementById('subtract');
+const multiplyPress = document.getElementById('multiply');
+const dividePress = document.getElementById('divide');
+const numsMath = document.getElementById('nums-math');
 
-// set event listeners to update state and DOM
+addPress.addEventListener('click', () => {
+    const number1 = Number(firstInput.value);
+    const number2 = Number(secondInput.value);
+    numsMath.textContent = number1 + number2;
+});
+
+subtractPress.addEventListener('click', () => {
+    const number1 = Number(firstInput.value);
+    const number2 = Number(secondInput.value);
+    numsMath.textContent = number1 - number2;
+});
+
+multiplyPress.addEventListener('click', () => {
+    const number1 = Number(firstInput.value);
+    const number2 = Number(secondInput.value);
+    numsMath.textContent = number1 * number2;
+});
+
+dividePress.addEventListener('click', () => {
+    const number1 = Number(firstInput.value);
+    const number2 = Number(secondInput.value);
+    numsMath.textContent = number1 / number2;
+});
